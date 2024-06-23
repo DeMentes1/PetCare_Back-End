25531c64a165902b9eae2f7ee458a8aff912f079
Armix18 <armix182002@gmail.com>
2024-06-23 00:44:45 -0500

    chore: initial commit

---
+++ src/main/java/pe/upc/petcarebackend/veterinaries/domain/model/entities/VeterinaryStatusItem.java
@@ -0,0 +1,13 @@
+package pe.upc.petcarebackend.veterinaries.domain.model.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import pe.upc.petcarebackend.shared.domain.model.entities.AuditableModel;
+
+@Getter
+@Entity
+public class VeterinaryStatusItem extends AuditableModel {
+/*
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)*/
+}
---
+++ src/test/java/pe/upc/petcarebackend/PetcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.petcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
